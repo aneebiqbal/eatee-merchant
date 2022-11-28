30727db1324c0c7c27e3b0254594cdb156fb44f4
aneebiqbal <1aneebiqbal@gmail.com>
2022-11-28 16:29:37 +0500

    minor fix

--- ios/foodleaMerchant/AppDelegate.m
+++ ios/foodleaMerchant/AppDelegate.m
@@ -10,7 +10,6 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-//#import "<SplashScreen.h>"
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -45,7 +44,6 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  //[SplashScreen show];
   return YES;
 }
 
