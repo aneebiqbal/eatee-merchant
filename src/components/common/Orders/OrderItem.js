f78b70e0e73d7036da76a275a1945afe20842b92
aneebiqbal <1aneebiqbal@gmail.com>
2023-02-02 19:40:07 +0500

    cancelOrder action call on OrdersScreen

--- src/components/common/Orders/OrderItem.js
+++ src/components/common/Orders/OrderItem.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import {Icon} from 'react-native-elements';
-import {Colors, Fonts, Images} from '../../../theme';
+import {ApplicationStyles, Colors, Fonts, Images} from '../../../theme';
 import ShadowCard from '../ShadowCard';
 import { CaptionedText } from '../CaptionedText';
 import Tag from '../Tag';
@@ -18,11 +18,11 @@ const OrderItem = ({
   navigation
 }) => {
   const [showItemDetails, setShowItemDetails] = useState(false);
-  const closeModal = () => setShowItemDetails(false)
+  const [isVisible, setIsVisible] = useState(false);  const closeModal = () => setShowItemDetails(false)
   const openModal = () => setShowItemDetails(true)
 
   return (
-    <ShadowCard style={styles.container}>
+    <ShadowCard style={styles.container} onPress={openModal}>
       <View style={styles.childContainer}>
         <View style={styles.orderIdStyles}>
           <Text style={styles.pr10}>
@@ -34,9 +34,7 @@ const OrderItem = ({
           <Text>Create date & time: </Text><Text>{order.createdOnText}</Text>
         </View>
       </View>
-      <View
-        style={styles.cardStyle}
-      >
+      <View style={styles.cardStyle}>
         <View style={styles.CardTextStyle}>
           <View style={styles.fdRow}>
             <Image 
@@ -50,13 +48,41 @@ const OrderItem = ({
           <CaptionedText heading={order.totalAmount} text={'total'} />
         </View>
 
-        <TouchableOpacity onPress={openModal}>
+        <TouchableOpacity onPress={() => setIsVisible(!isVisible)} style={{marginLeft: 10}}>
           <Icon name='dots-horizontal' type='material-community' color={Colors.primary} size={28} />
         </TouchableOpacity>
+
+        <View>
+          {isVisible && (
+            <View style={[modalStyles.container, ApplicationStyles.shadow]}>
+              {/* {children} */}
+              <TouchableOpacity onPress={() => setIsVisible(false)}>
+                <Text style={modalStyles.text}>Cancel Order</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
       </View>
       <OrderDetailsModal isVisible={showItemDetails} closeModal={closeModal} navigation={navigation} order={order}/>
     </ShadowCard>
   )
 }
 
+const modalStyles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 20,
+    right: 2,
+    height: 49,
+    width: 152,
+    marginLeft: 30,
+    backgroundColor: '#FFFFFF',
+    borderColor: 'rgba(7, 22, 49, 0.07)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+  },
+});
 export default OrderItem
